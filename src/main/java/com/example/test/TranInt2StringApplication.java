b57b91d6fe9e2952a7bd47e790147b80dd13459a
Nuttanun <006992@g-able.com>
2021-08-09 14:48:41 +0700

    Finish
    finish challenge

--- src/main/java/com/example/test/TranInt2StringApplication.java
+++ src/main/java/com/example/test/TranInt2StringApplication.java
@@ -1,13 +1,24 @@
 package com.example.test;
-
-import org.springframework.boot.SpringApplication;
+import java.util.Scanner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TranInt2StringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TranInt2StringApplication.class, args);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter String : ");
+		String str = sc.next();
+		
+		int zero = (int)'0';
+		int i = 0;
+		char[] st  = str.toCharArray();
+			for (char d : st) {
+				int n = (int)d;
+				if (n>= 48 && n <= 57) {
+					i = i*10 + (n-zero);
+				}
+			}
+		System.out.println("Integer : " + i);
 	}
-
 }
